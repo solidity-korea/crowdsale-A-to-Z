a907b0bf272d0d4f278cd63748145fa1967c2351
dongsam <dongsamb@gmail.com>
2018-05-11 03:44:13 +0900

    update FinalizableCrowdsale

--- FinalizableCrowdsaleExample.sol
+++ FinalizableCrowdsaleExample.sol
@@ -8,10 +8,20 @@ contract FinalizableCrowdsaleExample is FinalizableCrowdsale {
     uint256 _rate = 2;
     address _wallet = msg.sender;
     ERC20 _token = new SimpleToken();
+    uint256 _openingTime = now;
+    uint256 _closingTime = now + 60 * 10; // Sample Data, 10 min
 
     //constructor(uint256 _rate, address _wallet, ERC20 _token) public
     constructor() public
-    Crowdsale(_rate, _wallet, _token) {
+    Crowdsale(_rate, _wallet, _token)
+    TimedCrowdsale(_openingTime, _closingTime) {
 
     }
+
+    function finalization() internal {
+        _deliverTokens(owner, token.balanceOf(this));
+        //token.transfer(owner, token.balanceOf(this));
+        super.finalization();
+    }
+
 }
